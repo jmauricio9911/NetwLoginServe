a88c25de29a3ac8bca7f473ca8a1d7785df1a079
Yenifer <yeniferzapata107@gmail.com>
2019-03-29 11:35:40 -0500

    app of login

---
+++ routes/Users.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const users = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User')
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+//Registrar usuario
+users.post('/register', (req, res) => {
+  const today = new Date()
+  const userData = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    created: today
+  }
+
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    //Validamos si el usuario ya existe, si no existe procedemos a Crearlo
+    .then(user => {
+      if (!user) {
+        User.create(userData)
+          .then(user => {
+            let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+              expiresIn: 1440
+            })
+            res.json({ token: token })
+          })
+          .catch(err => {
+            res.send('error: ' + err)
+          })
+      } else {
+        res.json({ error: 'El usuario ya existe' })
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+//Validacion del login
+users.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+      password: req.body.password
+    }
+  })
+    .then(user => {
+      if (user) {
+        let token = jwt.sign(user.dataValues, process.env.SECRET_KEY, {
+          expiresIn: 1440
+        })
+        res.json({ token: token })
+      } else {
+        res.send('el usuario no existe')
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+//Ver perfil del usuario
+users.get('/profile', (req, res) => {
+  var decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+
+  User.findOne({
+    where: {
+      id: decoded.id
+    }
+  })
+    .then(user => {
+      if (user) {
+        res.json(user)
+      } else {
+        res.send('el usuario no existe')
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+
+module.exports = users
